72ae57a99d527120935a4bb4af4cdea2e926801d
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2023-10-15 17:41:16 +0530

    code fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -148,23 +148,12 @@ const DesktopSubNav = ({ label, href, subLabel }) => {
             _groupHover={{ color: "pink.400" }}
             fontWeight={500}
           >
-            <Link href={href}>{label}</Link>
+            <Link href={href} style={{textTransform: "capitalize"}}>{label}</Link>
           </Box>
-          <Box fontSize={"sm"}>
+          <Box fontSize={"sm"} >
             <Link href={href}> {subLabel}</Link>
           </Box>
         </Box>
-        <Flex
-          transition={"all .3s ease"}
-          transform={"translateX(-10px)"}
-          opacity={0}
-          _groupHover={{ opacity: "100%", transform: "translateX(0)" }}
-          justify={"flex-end"}
-          align={"center"}
-          flex={1}
-        >
-          <Icon color={"pink.400"} w={5} h={5} as={AiOutlineArrowRight} />
-        </Flex>
       </Stack>
     </Box>
   );
