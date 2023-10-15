4d0c67c9d3bcc9c6997d534a250b1c6f4233ecf1
Irfan Khan <officialirfanafzal@gmail.com>
2023-10-15 05:04:19 -0600

    Feat: Hero banner

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,13 @@ import Hero from "../../components/Hero";
 import Testimonials from "../../components/Testimonials";
 import Why from "../../components/Why";
 import Ourteam from "../../components/Ourteam";
+import HeroBanner from "../../components/HeroBanner";
 
 export default function Home() {
 	return (
 		<main>
 			<Hero />
+			<HeroBanner />
 			<Why />
 			<Expertise order="1" />
 			<Testimonials />
