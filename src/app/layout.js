3810ca9cc1e57815312ff868bceda9c8b82816bf
Irfan Khan <officialirfanafzal@gmail.com>
2023-10-15 02:15:38 -0600

    refactor: moved Footer to page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,5 @@
 import { Providers } from "./providers";
 import { Inter } from "next/font/google";
-import Navbar from "../../components/Navbar";
-import Footer from "../../components/Footer";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import Navbar from "../../components/Navbar";
 import Footer from "./components/Footer";
 
 export default function Home() {
 	return (
 		<main>
+			<Navbar />
 			<Footer />
 		</main>
 	);
