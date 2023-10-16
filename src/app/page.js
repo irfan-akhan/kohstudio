bfe522c5ac42ecd50a07f176272138ac00790eb4
Irfan Khan <officialirfanafzal@gmail.com>
2023-10-16 01:19:41 -0600

    Refactor: Hero section with image and text

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,12 @@
 import Expertise from "../../components/Expertise";
 import Clients from "../../components/Clients";
-import Hero from "../../components/Hero";
-import Testimonials from "../../components/Testimonials";
-import Why from "../../components/Why";
-import Ourteam from "../../components/Ourteam";
 import Gallery from "../../components/Gallery";
+import HeroBanner from "../../components/HeroBanner";
 
 export default function Home() {
 	return (
 		<main>
-			<Hero />
+			<HeroBanner />
 			{/* <Why /> */}
 			<Expertise />
 			{/* <Testimonials /> */}
