0e88450e02d3d5e57dba68c303805b25d4018c24
Irfan Khan <officialirfanafzal@gmail.com>
2023-10-19 06:47:38 -0600

    added clinet logos

--- components/Clients.jsx
+++ components/Clients.jsx
@@ -15,33 +15,59 @@ export default function Clients() {
 				alignItems={"center"}
 				justifyContent={"center"}
 			>
-				<Flex direction="column" justify="center" align="center">
-					<Image src={image} alt="icon" width={100} height={100} />
-					<Text mt="3">title here</Text>
+				<Flex direction="column" align="center">
+					<Image
+						src="/clients/cie.png"
+						alt="icon"
+						width={100}
+						height={100}
+					/>
+					<Text mt="3">CIE</Text>
 				</Flex>
-				<Flex direction="column" justify="center" align="center">
-					<Image src={image} alt="icon" width={100} height={100} />
-					<Text mt="3">title here</Text>
+				<Flex direction="column" align="center">
+					<Image
+						src="/clients/alpine.png"
+						alt="icon"
+						width={100}
+						height={100}
+					/>
+					<Text mt="3">Hotel Alpine Ridge</Text>
 				</Flex>
-				<Flex direction="column" justify="center" align="center">
-					<Image src={image} alt="icon" width={100} height={100} />
-					<Text mt="3">title here</Text>
+				<Flex direction="column" align="center">
+					<Image
+						src="/clients/pahalgam_hotel.jpg"
+						alt="icon"
+						width={100}
+						height={100}
+					/>
+					<Text mt="3">The Pahalgam Hotel</Text>
 				</Flex>
-				<Flex direction="column" justify="center" align="center">
-					<Image src={image} alt="icon" width={100} height={100} />
-					<Text mt="3">title here</Text>
+				<Flex direction="column" align="center">
+					<Image
+						src="/clients/ia.png"
+						alt="icon"
+						width={70}
+						height={70}
+					/>
+					<Text mt="3">IA Multiventures</Text>
 				</Flex>
-				<Flex direction="column" justify="center" align="center">
-					<Image src={image} alt="icon" width={100} height={100} />
-					<Text mt="3">title here</Text>
+				<Flex direction="column" align="center">
+					<Image
+						src="/clients/rawafid.jpg"
+						alt="icon"
+						width={100}
+						height={100}
+					/>
+					<Text mt="3">Rawafid Synergy</Text>
 				</Flex>
-				<Flex direction="column" justify="center" align="center">
-					<Image src={image} alt="icon" width={100} height={100} />
-					<Text mt="3">title here</Text>
-				</Flex>
-				<Flex direction="column" justify="center" align="center">
-					<Image src={image} alt="icon" width={100} height={100} />
-					<Text mt="3">title here</Text>
+				<Flex direction="column" align="center">
+					<Image
+						src="/clients/shaw.webp"
+						alt="icon"
+						width={100}
+						height={100}
+					/>
+					<Text mt="3">The Shaw</Text>
 				</Flex>
 			</Flex>
 		</Container>
