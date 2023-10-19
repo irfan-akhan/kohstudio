785912e5024f9d437064c3bdabb0e9ba9fb24ae4
Irfan Khan <officialirfanafzal@gmail.com>
2023-10-19 05:34:20 -0600

    font change to mont

--- src/app/providers.js
+++ src/app/providers.js
@@ -1,19 +1,19 @@
 "use client";
-import "@fontsource/ubuntu/400.css";
-import "@fontsource/ubuntu/500.css";
-import "@fontsource/ubuntu/700.css";
-import "@fontsource/roboto/700.css";
-import "@fontsource/roboto/900.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/400.css";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/700.css";
+import "@fontsource/montserrat/700.css";
+import "@fontsource/montserrat/900.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/400.css";
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
 	fonts: {
-		heading: `'Ubuntu', sans-serif`,
-		body: `'Roboto', sans-serif`,
+		heading: `'Montserrat', sans-serif`,
+		body: `'Montserrat', sans-serif`,
 	},
 });
 export function Providers({ children }) {
