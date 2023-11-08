bbb47a4290e5eca831827115d601c6d6ac625cef
Irfan Khan <officialirfanafzal@gmail.com>
2023-11-08 03:27:15 -0700

    added no careers

--- src/app/careers/page.js
+++ src/app/careers/page.js
@@ -1,18 +1,35 @@
-import { Container, Flex, Heading } from '@chakra-ui/react'
-import React from 'react'
-import Card from '../../../components/CareersCard'
+import {
+	Container,
+	Flex,
+	Card,
+	CardHeader,
+	CardBody,
+	Heading,
+	Text,
+} from "@chakra-ui/react";
+import React from "react";
+// import Card from "../../../components/CareersCard";
 
 export default function page() {
-  return (
-    <Container my="8rem" maxW="70rem">
-        <Heading textAlign="center" fontSize="3xl">
-            Careers
-        </Heading>
-        <Flex gap={10} flexWrap={"wrap"}>
+	return (
+		<Container my="8rem" maxW="70rem" centerContent>
+			<Heading textAlign="center" fontSize="3xl">
+				Careers
+			</Heading>
+      <Card>
+					<CardHeader>
+						{/* <Heading size="md">Join our team</Heading> */}
+					</CardHeader>
+					<CardBody>
+						<Text>No Openings at the moment</Text>
+					</CardBody>
+				</Card>
+			<Flex gap={10} flexWrap={"wrap"}>
+				
+				{/* <Card jobTittle="Senior Software Engineering" jobType="Fulltime" jobDomain="Engineering" joblocation="Srinagar" />
             <Card jobTittle="Senior Software Engineering" jobType="Fulltime" jobDomain="Engineering" joblocation="Srinagar" />
-            <Card jobTittle="Senior Software Engineering" jobType="Fulltime" jobDomain="Engineering" joblocation="Srinagar" />
-            <Card jobTittle="Senior Software Engineering" jobType="Fulltime" jobDomain="Engineering" joblocation="Srinagar" />
-        </Flex>
-    </Container>
-  )
+            <Card jobTittle="Senior Software Engineering" jobType="Fulltime" jobDomain="Engineering" joblocation="Srinagar" /> */}
+			</Flex>
+		</Container>
+	);
 }
